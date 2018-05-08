e6ffc49b219ed2972a6d653d5e237ad0a0de8d06
yuanl2 <yuanl2@cisco.com>
2018-05-08 17:05:20 +0800

    remove backend server code

--- service/server/src/main/java/com/hansun/server/metrics/TimedMetricsName.java
+++
@@ -1,9 +0,0 @@
-package com.hansun.server.metrics;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface TimedMetricsName {
-}
